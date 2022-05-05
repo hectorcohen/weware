5f166285d350917f4407853ac208d2575ac49254
hectorcohen <hectorcohenmaldonado@gmail.com>
2022-05-05 06:57:10 -0500

    add: eslint config

--- src/utils/images.jsx
+++ src/utils/images.jsx
@@ -1,35 +1,36 @@
-import bucketLogo from '../assets/logos/bucket.png'
-import lunaLogo from '../assets/logos/luna.png'
-import hyperiaLogo from '../assets/logos/hyperia.png'
-import leapcureLogo from '../assets/logos/leapcure.png'
-import betterWorksLogo from '../assets/logos/better-works.png'
-import meshLogo from '../assets/logos/mesh.png'
-import element from '../assets/images/element.png'
-import element_two from '../assets/images/element_2.png'
+import bucketLogo from '../assets/logos/bucket.png';
+import lunaLogo from '../assets/logos/luna.png';
+import hyperiaLogo from '../assets/logos/hyperia.png';
+import leapcureLogo from '../assets/logos/leapcure.png';
+import betterWorksLogo from '../assets/logos/better-works.png';
+import meshLogo from '../assets/logos/mesh.png';
+import element from '../assets/images/element.png';
+import elementTwo from '../assets/images/element_2.png';
 
-import asset from '../assets/images/mobile_asset.png'
-import asset_2 from '../assets/images/mobile_asset_2.png'
-import asset_3 from '../assets/images/mobile_asset_3.png'
-import asset_4 from '../assets/images/mobile_asset_4.png'
-import humman from '../assets/images/human.png'
-import asset_gradien_box from '../assets/images/asset_gradient_box.png'
+import asset from '../assets/images/mobile_asset.png';
+
+import assetsTwo from '../assets/images/mobile_asset_2.png';
+import assetThree from '../assets/images/mobile_asset_3.png';
+import assetFor from '../assets/images/mobile_asset_4.png';
+import humman from '../assets/images/human.png';
+import assetGradienBox from '../assets/images/asset_gradient_box.png';
 
 export const images = {
-    bucket: bucketLogo,
-    luna: lunaLogo,
-    hyperia: hyperiaLogo,
-    leapcure: leapcureLogo,
-    mesh: meshLogo,
-    better_works: betterWorksLogo,
-    element,
-    element_two,
-}
+  bucket: bucketLogo,
+  luna: lunaLogo,
+  hyperia: hyperiaLogo,
+  leapcure: leapcureLogo,
+  mesh: meshLogo,
+  better_works: betterWorksLogo,
+  element,
+  elementTwo,
+};
 
 export const assets = {
-    asset_one: asset,
-    asset_two: asset_2,
-    asset_three: asset_3,
-    asset_for: asset_4,
-    humman: humman,
-    asset_gradien_box
-}
+  assetOne: asset,
+  assetsTwo,
+  assetThree,
+  assetFor,
+  humman,
+  assetGradienBox,
+};
